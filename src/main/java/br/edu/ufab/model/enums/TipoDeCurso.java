cf24c2b267a1bf87d50eb2e116724c16e7f81c81
MuriloGustavo <murilogustavo98@gmail.com>
2018-05-03 20:02:11 -0300

    Hibernate, JPA e Spring de Curso

---
+++ src/main/java/br/edu/ufab/model/enums/TipoDeCurso.java
@@ -0,0 +1,7 @@
+package br.edu.ufab.model.enums;
+
+public enum TipoDeCurso {
+
+	GRADUACAO, POS_GRADUACAO
+	
+}
--- src/test/java/br/edu/ufab/testes/Testes.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ufab.testes;
-
-public class Testes {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
